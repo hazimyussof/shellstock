f1f608aecd26dd947f50f17383e4d409f1964adc
Hazim Yussof <hazqier205@gmail.com>
2024-04-10 17:43:12 +0800

    refactor: add ftxui template

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
-#include "./../include/getopt.h"
+// #include "./../include/getopt.h"
 
+// Included absolute path to remove annoying ftxui code errors from the lsp
+#include "./../build/_deps/ftxui-src/include/ftxui/dom/elements.hpp"
+#include "./../build/_deps/ftxui-src/include/ftxui/screen/screen.hpp"
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
+// int argc, char *argv[]
 int main(int argc, char *argv[]) {
-  getopt_out(argc, argv);
+  using namespace ftxui;
+
+  // Define the document
+  Element document = hbox({
+      text("left") | border,
+      text("middle") | border | flex,
+      text("right") | border,
+  });
+
+  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+  Render(screen, document);
+  screen.Print();
+
+  // getopt_out(argc, argv);
+
   return 0;
 }
